42f610b6a0ba288b7a9dde33948aab433a0ba179
António Cruz Júnior <antoniojr@icomp.ufam.edu.br>
2019-07-14 12:03:31 -0400

    finalize bootstrap settings

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -10,26 +10,18 @@ export class HomePageComponent implements OnInit {
   public banners: BannersInfo[] = [
     {
       src: 'assets/banners/banner_sib_passado.jpg',
-      title: 'SIB Manaus do passado',
-      subtitle: 'lorem ipsum',
-      alt: 'SIB Manaus do passado',
+      duration: 1000,
     },
     {
       src: 'assets/banners/banner_sib_presente.jpg',
-      title: 'SIB Manaus do passado',
-      alt: 'SIB Manaus do passado',
+      duration: 2000,
     },
     {
       src: 'assets/banners/banner_sib_futuro.jpg',
-      title: 'SIB Manaus do passado',
-      subtitle: 'lorem ipsum',
-      alt: 'SIB Manaus do passado',
+      duration: 3000,
     },
     {
       src: 'assets/banners/slide-arvore2.jpg',
-      title: 'SIB Manaus do passado',
-      subtitle: 'lorem ipsum',
-      alt: 'SIB Manaus do passado',
     },
     {
       src: 'assets/banners/slide-militares2.jpg',
