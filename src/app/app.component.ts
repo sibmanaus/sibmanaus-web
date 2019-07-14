93366574d2be32460f8858ef71e1421416c22cc7
António Cruz Júnior <antoniojr@icomp.ufam.edu.br>
2019-07-14 09:07:13 -0400

    feat: configure bootstrap, theme and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+declare const require: any;
+
+require('bootstrap');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portal-sib';
+  title = 'portal';
 }
